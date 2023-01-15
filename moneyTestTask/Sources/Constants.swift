26381418207e91b6225cbd3b883586f0a038dac5
David Tonoyan <davidtonoyan10@me.com>
2023-01-15 20:18:20 +0400

    Add constants

---
+++ moneyTestTask/Sources/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  moneyTestTask
+//
+//  Created by Давид Тоноян  on 15.01.2023.
+//
+
+import UIKit
+
+enum Constants {
+    enum Color {
+        static let white: UIColor = .white
+        
+    }
+}
