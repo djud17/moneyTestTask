67ddef04e060a23905dcb7290d7d4511ff84964c
David Tonoyan <davidtonoyan10@me.com>
2023-01-15 20:07:28 +0400

    Project setup

---
+++ moneyTestTask/Sources/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  moneyTestTask
+//
+//  Created by Давид Тоноян  on 15.01.2023.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
